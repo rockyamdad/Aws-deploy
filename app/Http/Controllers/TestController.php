4b77e14e989e39067973da17065626e7753ad2f5
rockyamdad <amdadul.hoque@sekai-lab-bd.com>
2019-11-28 13:24:09 +0600

    header line added

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,6 +10,7 @@ class TestController extends Controller
     public function show()
     {
         $date = Carbon::now();
+        echo '<h1> Test server: </h1>';
         echo $date->toDayDateTimeString();
     }
 }
